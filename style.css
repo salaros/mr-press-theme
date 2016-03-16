0f9e3629dd50a0954d0ed2b635cf82ede32cb036
Zhmayev Yaroslav <salaros@yandex.ru>
2016-03-16 22:31:53 +0300

    updated theme asset versions to v0.2.1

--- style.css
+++ style.css
@@ -1,6 +1,6 @@
 /* # Wordress-template-theme
 * Theme Name: Wordpress template theme
-* Template Version: 0.2
+* Template Version: 0.2.1
 * Author: Upstatement team
 * Author: Zhmayev Yaroslav <salaros@salaros.com>
 */
