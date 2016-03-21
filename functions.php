669d9642b77eaa0f3bafc55ff0d62f4420741c68
Zhmayev Yaroslav <salaros@yandex.ru>
2016-03-21 20:47:29 +0300

    improved Timber-related notice (both admin and non-admin)

--- functions.php
+++ functions.php
@@ -1,11 +1,21 @@
 <?php
 
 if ( ! class_exists( 'Timber' ) ) {
+
+    $admin_url = esc_url( admin_url( 'plugins.php' ) ) ;
+    $error_message = "
+    <div class=\"error alert alert-danger\">
+    <p>Timber not activated. Fix it here <a href=\"{$admin_url}\">{$admin_url}#timber</a></p>
+    </div>";
+
+    if ( !is_admin() ) {
+        die ($error_message);
+    }
+
     add_action( 'admin_notices', function() {
-        $admin_url = esc_url( admin_url( 'plugins.php' ) ) ;
-        $error_message = "<div class=\"error alert-danger\"><p>Timber not activated. Fix it here <a href=\"{$admin_url}\">{$admin_url}#timber</a></p></div>";
-        echo $error_message;
+        echo ($error_message);
     });
+
     return;
 }
 
