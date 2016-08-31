b7d6a7e4bed2d722861a8476c305d2e187e37c3b
Zhmayev Yaroslav <zhmayev.yaroslav@codecave.pro>
2016-08-31 12:08:46 +0300

    improved Bootstrapify class by adding tiny Twig templates for comment form elements

--- classes/Bootstrapify.php
+++ classes/Bootstrapify.php
@@ -14,32 +14,16 @@ class Bootstrapify {
         add_filter( 'comment_form_default_fields', function ( $fields ) {
 
             $commenter = wp_get_current_commenter();
+            $context = [
+                'require_name_email' => get_option( 'require_name_email' ),
+                'author_name' => esc_attr( @$commenter['comment_author'] ),
+                'author_email' => esc_attr( @$commenter['comment_author_email'] ),
+                'author_url' => esc_attr( @$commenter['comment_author_url'] ),
+            ];
 
-            $require_name_email = get_option( 'require_name_email' );
-            $aria_require_name_email = ( $require_name_email ) ? ' aria-required="true"' : '';
-            $require_name_email_attr = ( $require_name_email ) ? ' required' : '';
-
-            $author_name = esc_attr( @$commenter['comment_author'] );
-            $author_email = esc_attr( @$commenter['comment_author_email'] );
-            $author_url = esc_attr( @$commenter['comment_author_url'] );
-
-            $fields['author'] = sprintf(
-                '<div class="form-group comment-form-author form-control-wrapper label-floating empty">
-                    <label for="author" class="control-label">%s</label>
-                    <input class="form-control" id="author" name="author" type="text" autocomplete="off" value="%s" size="30" %s %s />
-                </div>', __('Name'), $author_url, $require_name_email_attr, $aria_require_name_email);
-
-            $fields['email'] = sprintf(
-                '<div class="form-group comment-form-email form-control-wrapper label-floating empty">
-                    <label for="email" class="control-label">%s</label>
-                    <input class="form-control" id="email" name="email" type="email" autocomplete="off" value="%s" size="30" %s %s />
-                </div>', __('Email'), $author_url, $require_name_email_attr, $aria_require_name_email);
-
-            $fields['url'] = sprintf(
-                '<div class="form-group comment-form-email form-control-wrapper label-floating empty">
-                    <label for="url" class="control-label">%s</label>
-                    <input class="form-control" id="url" name="url" type="url" autocomplete="off" value="%s" size="30" />
-                </div>', __('Website'), $author_url);
+            $fields['author'] = Timber::compile('comment-form-author.twig', $context);
+            $fields['email'] = Timber::compile('comment-form-email.twig', $context);
+            $fields['url'] = Timber::compile('comment-form-url.twig', $context);
 
             // unset($fields['url']);
 
@@ -47,17 +31,15 @@ class Bootstrapify {
         });
 
         add_filter( 'comment_form_defaults', function ( $args ) {
-            $args['class_submit'] = 'btn btn-primary btn-raised'; // since WP 4.1
+
+            $args['title_reply'] = "";
             $args['class_form'] = "comment-form";
 
-            $args['comment_notes_before'] = '';
-            $args['comment_notes_after'] = '';
+            $args['submit_button'] = Timber::compile('comment-form-submit.twig');
+            $args['comment_notes_before'] = Timber::compile('comment-form-notes-before.twig');
+            $args['comment_notes_after'] = Timber::compile('comment-form-notes-after.twig');
+            $args['comment_field'] = Timber::compile('comment-form-textarea.twig');
 
-            $args['comment_field'] = sprintf(
-                '<div class="form-group comment-form-comment form-control-wrapper label-floating empty">
-                    <label for="comment" class="control-label">%s</label>
-                    <textarea class="form-control" id="comment" name="comment" cols="45" autocomplete="off" rows="5" required aria-required="true"></textarea>
-                </div>', __('Your Comment'));
 
             return $args;
         });
