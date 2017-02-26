84cf7940c0774bece0d546d4612ade1e287587f2
salaros <salaros@yandex.ru>
2017-02-26 21:01:42 +0300

    just code formatting

--- classes/Bootstrapify.php
+++ classes/Bootstrapify.php
@@ -6,42 +6,42 @@ use \Timber as Timber;
 
 class Bootstrapify {
 
-    public static function init() {
-        self::tweak_comment_form();
-    }
+	public static function init() {
+		self::tweak_comment_form();
+	}
 
-    private static function tweak_comment_form() {
-        add_filter( 'comment_form_default_fields', function ( $fields ) {
+	private static function tweak_comment_form() {
+		add_filter( 'comment_form_default_fields', function ( $fields ) {
 
-            $commenter = wp_get_current_commenter();
-            $context = [
-                'require_name_email' => get_option( 'require_name_email' ),
-                'author_name' => esc_attr( @$commenter['comment_author'] ),
-                'author_email' => esc_attr( @$commenter['comment_author_email'] ),
-                'author_url' => esc_attr( @$commenter['comment_author_url'] ),
-            ];
+			$commenter = wp_get_current_commenter();
+			$context = [
+				'require_name_email' => get_option( 'require_name_email' ),
+				'author_name' => esc_attr( @$commenter['comment_author'] ),
+				'author_email' => esc_attr( @$commenter['comment_author_email'] ),
+				'author_url' => esc_attr( @$commenter['comment_author_url'] ),
+			];
 
-            $fields['author'] = Timber::compile('comment-form-author.twig', $context);
-            $fields['email'] = Timber::compile('comment-form-email.twig', $context);
-            $fields['url'] = Timber::compile('comment-form-url.twig', $context);
+			$fields['author'] = Timber::compile( 'comment-form-author.twig', $context );
+			$fields['email'] = Timber::compile( 'comment-form-email.twig', $context );
+			$fields['url'] = Timber::compile( 'comment-form-url.twig', $context );
 
-            // unset($fields['url']);
+			// unset($fields['url']);
 
-            return $fields;
-        });
+			return $fields;
+		});
 
-        add_filter( 'comment_form_defaults', function ( $args ) {
+		add_filter( 'comment_form_defaults', function ( $args ) {
 
-            $args['title_reply'] = "";
-            $args['class_form'] = "comment-form";
+			$args['title_reply'] = '';
+			$args['class_form'] = 'comment-form';
 
-            $args['submit_button'] = Timber::compile('comment-form-submit.twig');
-            $args['comment_notes_before'] = Timber::compile('comment-form-notes-before.twig');
-            $args['comment_notes_after'] = Timber::compile('comment-form-notes-after.twig');
-            $args['comment_field'] = Timber::compile('comment-form-textarea.twig');
+			$args['submit_button'] = Timber::compile( 'comment-form-submit.twig' );
+			$args['comment_notes_before'] = Timber::compile( 'comment-form-notes-before.twig' );
+			$args['comment_notes_after'] = Timber::compile( 'comment-form-notes-after.twig' );
+			$args['comment_field'] = Timber::compile( 'comment-form-textarea.twig' );
 
 
-            return $args;
-        });
-    }
+			return $args;
+		});
+	}
 }
--- classes/NavMenuTools.php
+++ classes/NavMenuTools.php
@@ -6,71 +6,80 @@ use Salaros\Wordpress\Template\TwigExtensions;
 
 class NavMenuTools {
 
-    public static function menu_exists( $menu_name ) {
-        $menu_obj = wp_get_nav_menu_object( $menu_name );
-        return ( $menu_obj && $menu_obj->term_id > 0 );
-    }
+	public static function menu_exists( $menu_name ) {
+		$menu_obj = wp_get_nav_menu_object( $menu_name );
+		return ( $menu_obj && $menu_obj->term_id > 0 );
+	}
 
-    public static function create_menu( $menu_name, $menu_items ) {
+	public static function create_menu( $menu_name, $menu_items ) {
 
-        if( self::menu_exists($menu_name) ) return false; // TODO log the error
+		if ( self::menu_exists( $menu_name ) ) {
+			return false; // TODO log the error
+		}
 
-        $menu_id = wp_create_nav_menu( $menu_name );
-        if( $menu_id <= 0 ) return false; // TODO log the error
+		$menu_id = wp_create_nav_menu( $menu_name );
+		if ( $menu_id <= 0 ) {
+			return false; // TODO log the error
+		}
 
-        foreach ( $menu_items as $menu_item ) {
+		foreach ( $menu_items as $menu_item ) {
 
-            $item = self::create_menu_item_obj($menu_item);
-            $item_id = wp_update_nav_menu_item( $menu_id, 0, $item );
-            if( $item_id <= 0 ) continue; // TODO log the error
+			$item = self::create_menu_item_obj( $menu_item );
+			$item_id = wp_update_nav_menu_item( $menu_id, 0, $item );
+			if ( $item_id <= 0 ) {
+				continue; // TODO log the error
+			}
 
-            if ( isset( $menu_item['data'] ) && is_array( $menu_item['data'] ) ) {
-                foreach( $menu_item['data'] as $meta_key => $meta_value ){
-                    update_post_meta($item_id, '_menu_item_'.$meta_key, $meta_value);
-                }
-            }
+			if ( isset( $menu_item['data'] ) && is_array( $menu_item['data'] ) ) {
+				foreach ( $menu_item['data'] as $meta_key => $meta_value ) {
+					update_post_meta( $item_id, '_menu_item_' . $meta_key, $meta_value );
+				}
+			}
 
-            if ( ! array_key_exists( 'children', $menu_item ) ) {
-                continue;
-            }
-            // create child items
-            foreach ( $menu_item['children'] as $child_item ) {
-                $subitem = self::create_menu_item_obj($child_item, $item_id);
-                $subitem_id = wp_update_nav_menu_item( $menu_id, 0, $subitem );
-                if( $subitem_id <= 0 ) continue; // TODO log the error
-            }
-        }
+			if ( ! array_key_exists( 'children', $menu_item ) ) {
+				continue;
+			}
 
-        return true;
-    }
+			// create child items
+			foreach ( $menu_item['children'] as $child_item ) {
+				$subitem = self::create_menu_item_obj( $child_item, $item_id );
+				$subitem_id = wp_update_nav_menu_item( $menu_id, 0, $subitem );
+				if ( $subitem_id <= 0 ) {
+					continue; // TODO log the error
+				}
+			}
+		}
 
-    public static function create_menu_item_obj( $menu_item, $parent_item_id = -1 ) {
+		return true;
+	}
 
-        if ( array_key_exists( 'slug', $menu_item ) ) {
-            $page = TwigExtensions::get_page_by_slug($menu_item['slug']);
-            $menu_item['id'] = $page->ID;
-            $menu_item['title'] = $page->title;
-        }
+	public static function create_menu_item_obj( $menu_item, $parent_item_id = -1 ) {
 
-        $item = [
-            'menu-item-title' =>  @$menu_item['title'],
-            'menu-item-classes' => @$menu_item['classes'],
-            'menu-item-status' => 'publish'
-        ];
+		if ( array_key_exists( 'slug', $menu_item ) ) {
+			$page = TwigExtensions::get_page_by_slug( $menu_item['slug'] );
+			$menu_item['id'] = $page->ID;
+			$menu_item['title'] = $page->title;
+		}
 
-        if ( array_key_exists( 'id', $menu_item ) ) {
-            $item['menu-item-object-id'] = $menu_item['id'];
-            $item['menu-item-object'] = 'page';
-            $item['menu-item-type'] = 'post_type';
-        } elseif ( array_key_exists( 'url', $menu_item ) ) {
-            $item['menu-item-url'] = $menu_item['url'];
-        } else {
-            $item['menu-item-url'] = '#';
-        }
+		$item = [
+			'menu-item-title' => @$menu_item['title'],
+			'menu-item-classes' => @$menu_item['classes'],
+			'menu-item-status' => 'publish',
+		];
 
-        if( $parent_item_id > 0 )
-            $item['menu-item-parent-id'] = $parent_item_id;
+		$item['menu-item-url'] = '#';
+		if ( array_key_exists( 'id', $menu_item ) ) {
+			$item['menu-item-object-id'] = $menu_item['id'];
+			$item['menu-item-object'] = 'page';
+			$item['menu-item-type'] = 'post_type';
+		} elseif ( array_key_exists( 'url', $menu_item ) ) {
+			$item['menu-item-url'] = $menu_item['url'];
+		}
 
-        return $item;
-    }
+		if ( $parent_item_id > 0 ) {
+			$item['menu-item-parent-id'] = $parent_item_id;
+		}
+
+		return $item;
+	}
 }
